825dc346fca095ec9437bda189d4f900a6ad278e
S Keerthana <keerthana.s2022d@vitstudent.ac.in>
2024-08-16 11:23:19 +0530

    add animation

---
+++ home.js
@@ -0,0 +1,38 @@
+function consoleText(word, id, color) {
+    var visible = true;
+    var letterCount = 1;
+    var waiting = false;
+    var target = document.getElementById(id);
+    var con = document.getElementById('console');
+
+    // Check if the elements exist
+    if (!target || !con) {
+        console.error("Target element or console element not found!");
+        return;
+    }
+
+    target.style.color = color;
+
+    function typeEffect() {
+        if (!waiting) {
+            target.innerHTML = word.substring(0, letterCount);
+            letterCount++;
+            if (letterCount > word.length) {
+                waiting = true;
+                setTimeout(function() {
+                    letterCount = 1;
+                    waiting = false;
+                }, 1000);
+            }
+        }
+    }
+
+    function blinkCursor() {
+        con.classList.toggle('hidden');
+    }
+
+    setInterval(typeEffect, 120);
+    setInterval(blinkCursor, 400);
+}
+
+consoleText('Show Off your Portfolio too!', 'text', 'black');
