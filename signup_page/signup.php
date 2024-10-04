3353f814e3204ae9df364813ffcc16aedc601c5f
S Keerthana <keerthana.s2022d@vitstudent.ac.in>
2024-10-04 23:09:51 +0530

    change some features

--- signup_page/signup.php
+++ signup_page/signup.php
@@ -1,4 +1,5 @@
 <?php
+    include('signup.html');
     $email = $_POST['email'];
     $uname = $_POST['uname'];
     $psw = $_POST['psw'];
@@ -9,6 +10,23 @@
     $pwd = "";
     $db = "LocalMart";
     $connect = mysqli_connect($host,$user,$pwd,$db);
-    $query = "INSERT INTO Users VALUES('$email','$uname','$psw')";
-    $result = mysqli_query($connect,$query);
+
+    $query1 = "SELECT * FROM Users WHERE USERNAME LIKE '$uname'";
+    $result1 = mysqli_query($connect,$query1);
+
+    if(mysqli_num_rows($result1) == 0){
+        if($psw == $psw_repeat)
+        {
+            $query = "INSERT INTO Users VALUES('$email','$uname','$psw')";
+            $result = mysqli_query($connect,$query);
+            header("location:../login.html");
+            exit;
+        }
+        else{
+            echo "<p> Passwords dont match. </p>";
+        }
+    }
+    else{
+        echo "<p> Username already exists. </p>";
+    }
 ?>
