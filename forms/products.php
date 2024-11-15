8afa3d66044fc316ff748d5d858e3229345b94f0
S Keerthana <keerthana.s2022d@vitstudent.ac.in>
2024-11-15 11:18:30 +0530

    modify products.php

--- forms/products.php
+++ forms/products.php
@@ -13,27 +13,21 @@
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Get product details from form submission
-    $productNames = $_POST['product_name'];  // Array of product names
-    $productDescriptions = $_POST['product_description'];  // Array of descriptions
-    $productPrices = $_POST['product_price'];  // Array of prices
-    $productTypes = $_POST['product_type'];  // Array of product types
+    $productNames = $_POST['product_name'];  
+    $productDescriptions = $_POST['product_description'];  
+    $productPrices = $_POST['product_price'];  
+    $productTypes = $_POST['product_type'];  
 
-    // Check if arrays are present
     if (is_array($productNames)) {
-        // Loop through the products
         for ($i = 0; $i < count($productNames) ; $i++) {
-            // Initialize an empty path for each product picture
             $picturePath = '';
 
-            // Check if a product picture was uploaded
             if (isset($_FILES['product_picture']['name'][$i]) && $_FILES['product_picture']['name'][$i] != '') {
                 $pictureName = $_FILES['product_picture']['name'][$i];
                 $pictureTmpName = $_FILES['product_picture']['tmp_name'][$i];
                 $uploadDirectory = "uploads/";
                 $picturePath = $uploadDirectory . basename($pictureName);
 
-                // Move the uploaded file to the designated directory
                 if (move_uploaded_file($pictureTmpName, $picturePath)) {
                     echo "Product picture " . ($i + 1) . " uploaded successfully.<br>";
                 } else {
@@ -41,11 +35,9 @@
                 }
             }
 
-            // Prepare SQL query to insert the product details into the database
             $query = "INSERT INTO products (Username,Product_Name, Product_Description, Product_Price, Product_Type, Product_Picture) 
                       VALUES ('$uname','$productNames[$i]', '$productDescriptions[$i]', '$productPrices[$i]', '$productTypes[$i]', '$picturePath')";
 
-            // Execute the query
             if (mysqli_query($connect, $query)) {
                 echo "Product " . ($i + 1) . " added successfully.<br>";
             } else {
@@ -56,6 +48,5 @@
         echo "No products found in the form.";
     }
 
-    // Close the database connection
     mysqli_close($connect);
 ?>
